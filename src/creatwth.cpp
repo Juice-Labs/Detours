f5c42f5f844455b749ee1644e82da32b46229cb9
Dean Beeler <dean.beeler@oculus.com>
2021-11-24 17:38:45 -0800

    Apply patch to support 64 to 32-bit patching.

--- src/creatwth.cpp
+++ src/creatwth.cpp
@@ -472,10 +472,30 @@ static BOOL RecordExeRestore(HANDLE hProcess, HMODULE hModule, DETOUR_EXE_RESTOR
 #undef IMAGE_NT_HEADERS_XX
 #undef IMAGE_NT_OPTIONAL_HDR_MAGIC_XX
 #undef IMAGE_ORDINAL_FLAG_XX
+#undef IMAGE_THUNK_DATAXX
 #undef UPDATE_IMPORTS_XX
+#undef DETOURS_BITS_XX
 #endif // DETOURS_32BIT
 
 #if DETOURS_64BIT
+
+#define DWORD_XX                        DWORD32
+#define IMAGE_NT_HEADERS_XX             IMAGE_NT_HEADERS32
+#define IMAGE_NT_OPTIONAL_HDR_MAGIC_XX  IMAGE_NT_OPTIONAL_HDR32_MAGIC
+#define IMAGE_ORDINAL_FLAG_XX           IMAGE_ORDINAL_FLAG32
+#define IMAGE_THUNK_DATAXX              IMAGE_THUNK_DATA32
+#define UPDATE_IMPORTS_XX               UpdateImports32
+#define DETOURS_BITS_XX                 32
+#include "uimports.cpp"
+#undef DETOUR_EXE_RESTORE_FIELD_XX
+#undef DWORD_XX
+#undef IMAGE_NT_HEADERS_XX
+#undef IMAGE_NT_OPTIONAL_HDR_MAGIC_XX
+#undef IMAGE_ORDINAL_FLAG_XX
+#undef IMAGE_THUNK_DATAXX
+#undef UPDATE_IMPORTS_XX
+#undef DETOURS_BITS_XX
+
 #define DWORD_XX                        DWORD64
 #define IMAGE_NT_HEADERS_XX             IMAGE_NT_HEADERS64
 #define IMAGE_NT_OPTIONAL_HDR_MAGIC_XX  IMAGE_NT_OPTIONAL_HDR64_MAGIC
@@ -835,10 +855,17 @@ BOOL WINAPI DetourUpdateProcessWithDllEx(_In_ HANDLE hProcess,
         return FALSE;
     }
 #elif defined(DETOURS_64BIT)
-    if (bIs32BitProcess || bIs32BitExe) {
-        // Can't detour a 32-bit process with 64-bit code.
-        SetLastError(ERROR_INVALID_HANDLE);
-        return FALSE;
+    if (bIs32BitProcess) {
+        if (bIs32BitExe) {
+            if (!UpdateImports32(hProcess, hModule, rlpDlls, nDlls)) {
+                return FALSE;
+            }
+        }
+        else {
+            // Can't detour a 32-bit process with 64-bit code.
+            SetLastError(ERROR_INVALID_HANDLE);
+            return FALSE;
+        }
     }
     else {
         // 64-bit native or 64-bit managed process on any platform.
